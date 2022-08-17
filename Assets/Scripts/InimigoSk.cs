cfe22bace49360572cf331bb3ee15a40cb443426
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-08-17 20:59:01 -0300

    Inimigos Animados

--- Assets/Scripts/InimigoSk.cs
+++ Assets/Scripts/InimigoSk.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoverHeroi : MonoBehaviour
+public class InimigoSk : MonoBehaviour
 {
     public List<Sprite> imgAndarCima;
     public List<Sprite> imgAndarBaixo;
@@ -12,42 +12,88 @@ public class MoverHeroi : MonoBehaviour
     public int indice = 0;
     public int contador = 0;
     public string texto = "";
+    public string destino = "";
+    public int posMin;
+    public int posMax;
+    public int contador2 = 0;
 
+    // Start is called before the first frame update
     void Start()
     {
         MostrarImagem = GetComponent<SpriteRenderer>();
+        destino = "Direita";
     }
 
     // Update is called once per frame
     void Update()
     {
         Movimento();
+        //Inteligencia();
+        InteligenciaRandomica();
+    }
+
+    void InteligenciaRandomica()
+    {
+        contador2 = contador2 + 1;
+        if (contador2 > 100)
+        {
+            contador2 = 0;
+            int numeroDestino = Random.Range(0, 4);
+            if(numeroDestino == 0)
+            {
+                destino = "Esquerda";
+            }
+            if (numeroDestino == 1)
+            {
+                destino = "Direita";
+            }
+            if (numeroDestino == 2)
+            {
+                destino = "Cima";
+            }
+            if (numeroDestino == 3)
+            {
+                destino = "Baixo";
+            }
+        }
+    }
+
+    void Inteligencia()
+    {
+        if (transform.position.x > posMax)
+        {
+            destino = "Esquerda";
+        }
+        if (transform.position.x < posMin)
+        {
+            destino = "Direita";
+        }
     }
 
 
     void Movimento()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (destino == "Esquerda")
         {
             transform.position = new Vector3(transform.position.x - 0.002f, transform.position.y, transform.position.z);
             texto = "Esquerda";
             Animacao();
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (destino == "Direita")
         {
             transform.position = new Vector3(transform.position.x + 0.002f, transform.position.y, transform.position.z);
             texto = "Direita";
             Animacao();
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (destino == "Cima")
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y+0.002f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.002f, transform.position.z);
             texto = "Cima";
             Animacao();
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (destino == "Baixo")
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y-0.002f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.002f, transform.position.z);
             texto = "Baixo";
             Animacao();
         }
@@ -56,20 +102,20 @@ public class MoverHeroi : MonoBehaviour
     void Animacao()
     {
         contador = contador + 1;
-        if(contador > 50)
+        if (contador > 50)
         {
             indice = indice + 1;
-            if(indice > 4)
+            if (indice > 4)
             {
                 indice = 0;
             }
             contador = 0;
         }
-        if(texto == "Cima")
+        if (texto == "Cima")
         {
             MostrarImagem.sprite = imgAndarCima[indice];
         }
-        if(texto == "Baixo")
+        if (texto == "Baixo")
         {
             MostrarImagem.sprite = imgAndarBaixo[indice];
         }
@@ -83,5 +129,4 @@ public class MoverHeroi : MonoBehaviour
         }
 
     }
-    
 }
